799a592810d59152ace4feae7fbc2209a5a73b4f
juwon <asehindej@gmail.com>
2025-06-18 00:25:58 +0100

    feat: add getUserHabits function to retrieve habits for a specific user

--- controller/Habit.js
+++ controller/Habit.js
@@ -1,8 +1,9 @@
 import { HabitModel } from "../model/HabitModel.js";
+import { UserModel } from "../model/UserModel.js";
 import connectDb from "../utils/connectDb.js";
 
 const createHabit = async (req, res) => {
-  await connectDb(); // Ensure DB connection
+  await connectDb();
 
   try {
     const { userId, title, description, frequency, startDate } = req.body;
@@ -28,4 +29,26 @@ const createHabit = async (req, res) => {
   }
 };
 
-export { createHabit };
+const getUserHabits = async (req, res) =>{
+  await connectDb();
+  try {
+    const {userId} = req.params;
+    if(!userId){
+      return res.status(400).json({ error: "Request with no data" });
+    }
+
+    const user = await UserModel.findById(userId);
+    if(!user){
+      return res.status(400).json({ error: "User not authenticated" });
+    };
+
+    const userHabits = await HabitModel.find({userId});
+    res.status(201).json(userHabits);
+    
+  } catch (error) {
+    console.log("getHabitErro:", error);
+    return res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+export { createHabit, getUserHabits };
--- router/HabitRoute.js
+++ router/HabitRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createHabit } from "../controller/Habit.js";
+import { createHabit, getUserHabits } from "../controller/Habit.js";
 
 
 export const HabitRouter = express.Router();
 
-HabitRouter.use("/create", createHabit);
+HabitRouter.post("/create", createHabit);
+HabitRouter.get("/user-habits/:userId", getUserHabits);
